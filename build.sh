683dbc5551985c07b259e38fadef879d2e3731e9
templeobijr <thealternativechain@gmail.com>
2025-04-15 13:49:15 +0300

    updated for static files

---
+++ build.sh
@@ -0,0 +1,8 @@
+==== FILE START =====
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
+==== FILE END ==== 
